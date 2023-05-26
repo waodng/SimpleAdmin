f9c90683248d10c0c835dadf52e7f17bff68284b
huguodong <531035580@qq.com>
2023-05-26 14:01:11 +0800

    【升级】#I77CJ2 调整架构，实现真插件式开发

---
+++ api/SimpleAdmin/SimpleAdmin.SqlSugar/ExtJson/RelationRoleResuorce.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleAdmin.SqlSugar;
+
+/// <summary>
+/// SYS_ROLE_HAS_RESOURCE
+/// 角色有哪些资源扩展
+/// </summary>
+public class RelationRoleResuorce
+{
+    /// <summary>
+    /// 菜单ID
+    /// </summary>
+    public string MenuId { get; set; }
+
+    /// <summary>
+    /// 按钮信息
+    /// </summary>
+    public List<string> ButtonInfo { get; set; } = new List<string>();
+}
